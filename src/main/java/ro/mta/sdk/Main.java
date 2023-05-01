40f36732a3d255824417a5155da00d6f3fb18d8e
Rosu Cosmin <cosmin.rosu@mta.ro>
2023-05-01 22:42:39 +0300

    More testing

--- src/main/java/ro/mta/sdk/Main.java
+++ src/main/java/ro/mta/sdk/Main.java
@@ -3,11 +3,15 @@ package ro.mta.sdk;
 public class Main {
     public static void main(String[] args) {
         ToggleSystemConfig toggleSystemConfig = ToggleSystemConfig.builder()
-                .toggleServerAPI("http://localhost:8080/api")
-                .apiKey("dadada")
+                .toggleServerAPI("http://localhost:8080")
+                .instanceId("instance1")
+                .apiKey("2:1.44B72BDC152F3D1214B84CA5D923CA917AD14E1B48713EE2CEC2F3F0B339E3A")
+                .synchronousFetchOnInitialisation(true)
                 .pollingInterval(2)
-                .appName("app")
+                .appName("demo")
                 .build();
+
+        System.out.println(toggleSystemConfig.getInstanceId());
 
         ToggleSystemContextProvider toggleSystemContextProvider = ToggleSystemContextProvider.getDefaultProvider();
         ToggleSystemContext toggleSystemContext = toggleSystemConfig.getToggleSystemContextProvider().getContext();
@@ -16,7 +20,13 @@ public class Main {
         ToggleSystemContext context = ToggleSystemContext.builder()
                 .userId("user@mail.com").build();
 
+
         ToggleSystemClient systemClient = new ToggleSystemClient(toggleSystemConfig);
+        if(systemClient.isEnabled("nume5")) {
+            System.out.println("DA");
+        } else {
+            System.out.println("NU");
+        }
         while (true){
 
         }
